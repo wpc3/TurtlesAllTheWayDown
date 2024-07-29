f2b8481e007277dee94dfb1ad8e2d6d4f8c30581
Will <will@Zipcoders-MacBook-Pro-9.local>
2024-07-29 09:02:01 -0400

    made changes

---
+++ src/test/java/GCD.java
@@ -0,0 +1,21 @@
+public class GCD {
+
+
+        // recursive implementation
+        public static int gcd(int p, int q) {
+            if (q == 0) return p;
+            else return gcd(q, p % q);
+        }
+
+        //non-recursive
+    public static int gcd2(int p, int q) {
+        while (q != 0) {
+            int temp = q;
+            q = p % q;
+            p = temp;
+        }
+        return p;
+    }
+
+
+}
