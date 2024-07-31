97b39506c1de72556f1a6b711e2a58d368f3e754
Will <will@Zipcoders-MacBook-Pro-9.local>
2024-07-31 08:08:28 -0400

    done

--- src/test/java/GCD.java
+++ src/test/java/GCD.java
@@ -20,13 +20,35 @@ public class GCD {
         return p;
     }
 
-    public static long compareMethod(){
-        Instant start = Instant.now();
-        // CODE to be timed goes HERE
+//    public static long compareMethod(){
+//        Instant start = Instant.now();
+//        // CODE to be timed goes HERE
+//
+//        Instant finish = Instant.now();
+//        long timeElapsed = Duration.between(start, finish).toMillis();
+//        return timeElapsed;
+//    }
 
+    public static void main(String[] args) {
+        int a = 900000000;
+        int b = 300000000;
+
+        Instant start = Instant.now();
+        int gcd1 = GCD.gcd(a,b);
         Instant finish = Instant.now();
-        long timeElapsed = Duration.between(start, finish).toMillis();
-        return timeElapsed;
+        long timeElasped = Duration.between(start,finish).toMillis();
+        System.out.println("GCD: " + gcd1);
+        System.out.println("Time taken for GCD1 " + timeElasped);
+
+
+         start = Instant.now();
+        int gcd2 = GCD.gcd2(a,b);
+        finish = Instant.now();
+         timeElasped = Duration.between(start,finish).toMillis();
+        System.out.println("GCD2: " + gcd2);
+        System.out.println("Time taken for GCD2 " + timeElasped);
+
+
     }
 
 }
